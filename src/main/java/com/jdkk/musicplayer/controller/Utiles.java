171d445b1824ec49b9c5729c5813b4bb10a0800b
Rodrigo <rodrigoolguin8@gmail.com>
2026-07-06 14:23:36 -0400

    Primer commit

---
+++ src/main/java/com/jdkk/musicplayer/controller/Utiles.java
@@ -0,0 +1,16 @@
+package com.jdkk.musicplayer.controller;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Utiles {
+    public static void clearConsole() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+    }
+
+
+}
